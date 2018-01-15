5bf6979d447870a2e5ac35494e4053385db77466
Vish <vish@agilityroots.com>
2018-01-15 09:58:00 +0530

    #11 WIP - elastic-kibana vagrant box tested working. - need to create packer recipes and distributable box files

---
+++ elk/provisioning/environments/production/manifests/default.pp
@@ -0,0 +1,22 @@
+# https://forge.puppet.com/elastic/elasticsearch
+
+include java
+class { 'elasticsearch':
+  version => '6.0.0',
+  restart_on_change => true,
+  instances => {
+    'es-01' => {
+      'config' => {
+        'network.host' => '0.0.0.0'
+      }
+    }
+  }
+}
+
+class { 'kibana':
+  ensure => '6.0.0',
+  config => {
+    'server.host' => '0.0.0.0',
+    'server.port' => '5601',
+  }
+}
